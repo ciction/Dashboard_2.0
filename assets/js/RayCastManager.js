1866e7ba5af128c09ba5280ac1d06bad63496912
Swolfs <cswolfs@vub.ac.be>
2017-02-19 09:58:52 +0100

    raycast update, stabieler selecteren (geen turbines meer die per ongeluk geslecteerd blijven wanner je snel van de ene naar de andere hovert door beter algoritme)

--- assets/js/RayCastManager.js
+++ assets/js/RayCastManager.js
@@ -10,18 +10,21 @@ class RayCastManager{
         //RAYCASTING
         RayCastManager.ms_Raycaster.setFromCamera(WINDOW.ms_Mouse, this.ms_Camera);
         var intersects = RayCastManager.ms_Raycaster.intersectObjects(this.ms_Scene.children, true);
+
+        //unselect previous if the raycast is hovering over a new object
+        if(RayCastManager.ms_SelectedTurbine != null){
+            if(intersects[0].object != RayCastManager.ms_SelectedTurbine){
+                RayCastManager.ms_SelectedTurbine.highLight(0x000000);
+                RayCastManager.ms_SelectedTurbine = null;
+            }
+        }
+        //select and highlight a turbine when it is hovered
         if(intersects[0].object.userData.baseType == 'turbine'){
             var nr                  = intersects[0].object.userData.Nr;
             var type                = intersects[0].object.userData.type;
             //console.log('turbine selected: ' + nr + " type: " + type);
             RayCastManager.ms_SelectedTurbine = Turbine.TurbineList[nr-1];
             RayCastManager.ms_SelectedTurbine.highLight(0x9999bb);
-        }
-        else{
-            if(RayCastManager.ms_SelectedTurbine){
-                RayCastManager.ms_SelectedTurbine.highLight(0x000000);
-                RayCastManager.ms_SelectedTurbine = null;
-            }
         }
     }
 
